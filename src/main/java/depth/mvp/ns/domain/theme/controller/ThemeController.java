bb7228eba0fb88d704fccbc9a9602f9aa100150d
LeeJiSoo <ljs08010@gmail.com>
2024-08-11 22:22:37 +0900

    [REMOVE] 불필요한 코드 제거

--- src/main/java/depth/mvp/ns/domain/theme/controller/ThemeController.java
+++ src/main/java/depth/mvp/ns/domain/theme/controller/ThemeController.java
@@ -5,12 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
-
 
 @RequiredArgsConstructor
 @RestController
@@ -40,7 +35,7 @@ public class ThemeController {
         return themeService.searchTheme(keyword, pageable);
     }
 
- }
+
     @GetMapping("/{themeId}")
     public ResponseEntity<?> getThemeDetail(
             @PathVariable Long themeId,
