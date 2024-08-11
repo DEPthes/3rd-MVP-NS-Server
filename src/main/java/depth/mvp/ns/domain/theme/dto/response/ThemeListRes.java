07ca250ed081d52efc24d0f6b0c20aed7fef8840
LeeJiSoo <ljs08010@gmail.com>
2024-08-11 20:01:32 +0900

    [REFACTOR] api응답 date타입 변경

--- src/main/java/depth/mvp/ns/domain/theme/dto/response/ThemeListRes.java
+++ src/main/java/depth/mvp/ns/domain/theme/dto/response/ThemeListRes.java
@@ -4,12 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 public class ThemeListRes {
     private Long themeId; // 주제ID
     private String content; // 주제 내용
-    private LocalDate date; // 주제 발행일
+    private String date; // 주제 발행일
     private int likeCount; // 주제 좋아요수
     private int boardCount; // 게시글 수
 
@@ -17,7 +18,7 @@ public class ThemeListRes {
     ThemeListRes(Long themeId, String content, LocalDate date, int likeCount, int boardCount){
         this.themeId = themeId;
         this.content = content;
-        this.date = date;
+        this.date = date.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
         this.likeCount = likeCount;
         this.boardCount = boardCount;
     }
