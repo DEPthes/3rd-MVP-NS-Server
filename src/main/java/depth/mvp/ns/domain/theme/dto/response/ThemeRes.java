a6f67a5f89d57e7d02f6a0857e0e22c9e4ca43e3
LeeJiSoo <ljs08010@gmail.com>
2024-08-18 23:33:31 +0900

    [REFACTOR] 중복 코드 추출 메서드 생성

--- src/main/java/depth/mvp/ns/domain/theme/dto/response/ThemeRes.java
+++ src/main/java/depth/mvp/ns/domain/theme/dto/response/ThemeRes.java
@@ -4,14 +4,14 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class TodayThemeRes {
+public class ThemeRes {
     private Long themeId; // 주제ID
     private String content; // 오늘의 주제
     private Long userId; // 사용자ID
     private boolean likedTheme; // 주제 좋아요 여부
 
     @Builder
-    public TodayThemeRes(Long themeId,String content, Long userId, boolean likedTheme){
+    public ThemeRes(Long themeId,String content, Long userId, boolean likedTheme){
         this.themeId = themeId;
         this.content = content;
         this.userId = userId;
