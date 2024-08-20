c3d8cf46af9e10ce4c0c5cef630c4de60b53aa74
LeeJiSoo <ljs08010@gmail.com>
2024-08-20 16:43:49 +0900

    [FEAT] 주제 상세 조회 시 발행된 게시글만 조회할 수 있는 조건 추가(쿼리수정)

--- src/main/java/depth/mvp/ns/domain/board/domain/repository/BoardRepository.java
+++ src/main/java/depth/mvp/ns/domain/board/domain/repository/BoardRepository.java
@@ -18,12 +18,12 @@ public interface BoardRepository extends JpaRepository<Board, Long>, BoardQueryD
     @Query("SELECT COUNT(l) FROM BoardLike l WHERE l.board.id = :boardId AND l.status = 'ACTIVE'")
     int countLikesByBoardId(@Param("boardId") Long boardId);
 
-    @Query("SELECT b FROM Board b WHERE b.theme.id = :themeId ORDER BY b.createdDate DESC")
-    Page<Board> findByThemeIdOrderByDate(@Param("themeId") Long themeId, Pageable pageable);
+    @Query("SELECT b FROM Board b WHERE b.theme.id = :themeId AND b.isPublished = true  ORDER BY b.createdDate DESC")
+    Page<Board> findByThemeIdAndIsPublishedTrueOrderByDate(@Param("themeId") Long themeId, Pageable pageable);
 
-    @Query("SELECT b FROM Board b WHERE b.theme.id = :themeId ORDER BY (SELECT COUNT(bl) FROM BoardLike bl " +
+    @Query("SELECT b FROM Board b WHERE b.theme.id = :themeId AND b.isPublished = true ORDER BY (SELECT COUNT(bl) FROM BoardLike bl " +
             "WHERE bl.board.id = b.id AND bl.status = 'ACTIVE') DESC")
-    Page<Board> findByThemeIdOrderByLikeCount(@Param("themeId") Long themeId, Pageable pageable);
+    Page<Board> findByThemeIdAndIsPublishedTrueOrderByLikeCount(@Param("themeId") Long themeId, Pageable pageable);
 
     @Query("SELECT COUNT(b) FROM Board b WHERE b.theme = :theme AND b.isPublished = :isPublished")
     int countByThemeAndIsPublished(Theme theme, boolean isPublished);
