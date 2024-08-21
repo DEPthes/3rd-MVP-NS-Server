7fad88b7ad6fb8b1961a30698690386dd400be91
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-08-21 17:55:05 +0900

    [FEAT] 글 발행 시 현재 주제인지 응답 추가

--- src/main/java/depth/mvp/ns/domain/board/dto/response/PublishBoardRes.java
+++ src/main/java/depth/mvp/ns/domain/board/dto/response/PublishBoardRes.java
@@ -9,11 +9,14 @@ import lombok.NoArgsConstructor;
 public class PublishBoardRes {
 
     private String message; // 성공 메세지
-    private boolean firstPost;
+    private boolean firstPost;   // 첫번째 게시글 여부
+    private boolean todayTheme;  // 현재 주제 여부
+
 
     @Builder
-    public PublishBoardRes(String message, boolean firstPost){
+    public PublishBoardRes(String message, boolean firstPost, boolean todayTheme){
         this.message = message;
         this.firstPost = firstPost;
+        this.todayTheme = todayTheme;
     }
 }
